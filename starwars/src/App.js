abac313e8d5b1038d055dfecbcf480a31ac7f4a4
Germaine Pannell <me@germainep.com>
2019-10-27 20:16:05 -0400

    created a charcter card component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
+import {container } from 'reactstrap';
 
 import { useState, useEffect } from 'react';
 
@@ -26,6 +27,9 @@ useEffect(()=> {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Container>
+        
+      </Container>
     </div>
   );
 }
